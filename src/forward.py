79ebc51d0bd78701c6fa1e2c8a3fbf43d26ca53c
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-05-12 08:33:16 +0200

    add deciphering

--- src/forward.py
+++ src/forward.py
@@ -5,26 +5,57 @@ import socket
 import sys
 import time
 from argparse import ArgumentParser
-from base64 import b64encode
+from base64 import b64decode, b64encode
 from dataclasses import dataclass
 from pathlib import Path
 
 
 @dataclass(frozen=True)
-class Config:
+class Record:
     target: Path
 
 
-def parse_args() -> Config:
+@dataclass(frozen=True)
+class Replay:
+    target: Path
+
+@dataclass(frozen=True)
+class Decipher:
+    log: Path
+
+def parse_args() -> Record | Replay | Decipher:
     parser = ArgumentParser(description="Replace path and log")
-    parser.add_argument(
+    command_parser = parser.add_subparsers(dest="command", required=True)
+    record = command_parser.add_parser("record")
+    record.add_argument(
         "--target",
         type=Path,
         required=True,
-        help="The target path to replace",
+        help="target path to replace",
+    )
+    replay = command_parser.add_parser("replay")
+    replay.add_argument(
+        "--target",
+        type=Path,
+        required=True,
+        help="rrdcached.sock",
+    )
+    decipher = command_parser.add_parser("decipher")
+    decipher.add_argument(
+        "--log",
+        type=Path,
+        default='datalog.jsonl',
+        help="datalog.jsonl",
     )
     namespace = parser.parse_args()
-    return Config(target=namespace.target)
+    match namespace.command:
+        case "record":
+            return Record(target=namespace.target)
+        case "replay":
+            return Replay(target=namespace.target)
+        case "decipher":
+            return Decipher(log=namespace.log)
+    raise NotImplementedError()
 
 
 class Console:
@@ -36,16 +67,12 @@ class Console:
             print(message)
 
 
-def _serialize(message: bytes) -> str | list[float]:
+def _serialize(message: bytes) -> str:
     return b64encode(message).decode("ascii")
 
 
-def _get_complete_message(data: bytes) -> tuple[list[bytes], bytes]:
-    # ascii protocol seperated by new lines.
-    if data.endswith(b"\n") or not data:
-        return data.splitlines(), b""
-    *messages, trailing = data.splitlines()
-    return messages, trailing
+def _deserialize(message: str) -> bytes:
+    return b64decode(message.encode("ascii"))
 
 
 class DataLog:
@@ -87,7 +114,7 @@ def _handle_connection(
         conn.setblocking(False)
         for sock in readable:
             console.log("recv: " + ("rrdcached" if sock == conn else "cmc"))
-            data = sock.recv(2 * 32)
+            data = sock.recv(2**32)
             if sock == conn:
                 console.log(f">> {data.decode('ascii')}")
                 data_log.log_send(data)
@@ -107,10 +134,32 @@ def _handle_connection(
                 else:
                     sys.exit(1)
 
+def _get_complete_message(data: bytes) -> tuple[list[bytes], bytes]:
+    # ascii protocol seperated by new lines.
+    if data.endswith(b"\n") or not data:
+        return data.splitlines(), b""
+    *messages, trailing = data.splitlines()
+    return messages, trailing
 
-def main() -> None:
-    config = parse_args()
-    console = Console(False)
+
+def decipher(console: Console, config: Decipher) -> None:
+    with config.log.open() as file:
+        for line in file:
+            log = json.loads(line)
+            console.log(log["type_"])
+            messages, trailing = _get_complete_message(_deserialize(log["message"]))
+            for message in messages:
+                try:
+                    readable = message.decode('ascii')
+                except:
+                    readable = "<unreadable>"
+                console.log(readable)
+
+
+
+
+
+def record(console: Console, config: Record) -> None:
     original = config.target.with_suffix(".original")
     config.target.rename(original)
     try:
@@ -132,6 +181,37 @@ def main() -> None:
                     i += 1
     finally:
         original.rename(config.target)
+
+
+def replay(console: Console, config: Replay) -> None:
+    with (
+        open("datalog.jsonl") as file,
+        socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock,
+    ):
+        sock.connect(str(config.target))
+        for i, line in enumerate(file):
+            console.log(f"{i}: {line}")
+            log = json.loads(line)
+            message = _deserialize(log["message"])
+            match log["type_"]:
+                case "send":
+                    console.log(f">> {message.decode('ascii')}")
+                    sock.sendall(message)
+                case "recv":
+                    data = sock.recv(2**32)
+                    console.log(f"<< count: {len(data)}")
+
+
+def main() -> None:
+    config = parse_args()
+    console = Console(True)
+    match config:
+        case Record():
+            record(console, config)
+        case Replay():
+            replay(console, config)
+        case Decipher():
+            decipher(console, config)
 
 
 if __name__ == "__main__":
