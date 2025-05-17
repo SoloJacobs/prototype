59018d939ab6c126b310d0c1c114c4a9f8f33057
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-05-17 12:03:31 +0200

    add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Arguments {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Command {
+    Record {
+        #[clap(long, short)]
+        output: PathBuf,
+        #[clap(long, short)]
+        socket: PathBuf,
+    },
+    Replay {
+        #[clap(long, short)]
+        input: PathBuf,
+        #[clap(long, short)]
+        socket: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::parse();
+    println!("arg: {arguments:?}");
 }
