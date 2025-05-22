0cdfec8727ab605576556e462cf3e6ad87d6fa97
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-05-22 11:32:14 +0200

    script rrd file creation

---
+++ src/iterate.py
@@ -0,0 +1,90 @@
+import os
+from dataclasses import dataclass
+from pathlib import Path
+
+import rrdtool 
+
+
+@dataclass(frozen=True)
+class RRDInfo:
+    host: str
+    service: str
+    metrics: list[str]
+
+
+def _parse_cmc_rrd_info(info_file_path: str) -> RRDInfo:
+    with open(info_file_path) as file:
+        content = file.read()
+    host_line, service_line, metrics_line = content.splitlines()
+    return RRDInfo(
+        host=host_line[5:],
+        service=service_line[8:],
+        metrics=metrics_line[8:].split(";"),
+    )
+
+
+def pnp_cleanup(s: str) -> str:
+    """Quote a string (host name or service name) in PNP4Nagios format
+
+    Because it is used as path element, this needs to be handled as "str" in Python 2 and 3
+    """
+    return s.replace(" ", "_").replace(":", "_").replace("/", "_").replace("\\", "_")
+
+
+def _rrd_cmc_host_dir(hostname: str) -> str:
+    # We need /opt here because of bug in rrdcached
+    return str(Path("/tmp/rrd") / pnp_cleanup(hostname))
+
+
+# RRDSpec(
+#     format = "cmc_single"
+#     host: HostName
+#     service: _RRDServiceName
+#     metrics: Sequence[tuple[str, str | None]]
+
+
+def _create_rrd(info: RRDInfo) -> str:
+    heartbeat = 8460
+    step = 60
+    host_dir = _rrd_cmc_host_dir(info.host)
+    base_file_name = host_dir + "/" + pnp_cleanup(info.service)
+
+    rrd_file_name = base_file_name + ".rrd"
+
+    os.makedirs(host_dir, exist_ok=True)
+
+    args = [rrd_file_name, "--step", str(step)]
+    for nr, _varname in enumerate(info.metrics, 1):
+        args.append(f"DS:{nr}:GAUGE:{heartbeat}:U:U")
+    args += [
+        "RRA:AVERAGE:0.50:1:2880",
+        "RRA:AVERAGE:0.50:30:4320",
+        "RRA:AVERAGE:0.50:360:5840",
+        "RRA:AVERAGE:0.50:5:2880",
+        "RRA:MAX:0.50:1:2880",
+        "RRA:MAX:0.50:30:4320",
+        "RRA:MAX:0.50:360:5840",
+        "RRA:MAX:0.50:5:2880",
+        "RRA:MIN:0.50:1:2880",
+        "RRA:MIN:0.50:30:4320",
+        "RRA:MIN:0.50:360:5840",
+        "RRA:MIN:0.50:5:2880",
+    ]
+
+    rrdtool.create(*args)
+
+    return rrd_file_name
+
+
+def main() -> None:
+    rrd = "/home/solo/git/prototype/rrd/"
+    for host in os.listdir(rrd):
+        host_dir = f"rrd/{host}"
+        for rrd in os.listdir(host_dir):
+            if rrd.endswith(".info"):
+                rrd_file = f"{host_dir}/{rrd}"
+                info = _parse_cmc_rrd_info(rrd_file)
+                _create_rrd(info)
+
+
+main()
