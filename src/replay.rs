d95585d57c536e2cab8f24c2740f888fdf324aec
Solomon Jacobs <solomon.jacobs@checkmk.com>
2025-06-03 18:21:52 +0200

    failed experiment of partitioning pg by hand
    Total time taken for creating the partitions:
    ```
    2025-06-03T16:18:12.396618Z  INFO replay: processed 1000
    2025-06-03T16:21:25.341075Z  INFO replay: processed 2000
    ```
    Note, that we need around `20_000_000` metrics, and we are not even
    storing any meta information.

--- src/replay.rs
+++ src/replay.rs
@@ -1,15 +1,16 @@
 use base64::prelude::*;
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, Utc};
 use clap::{ArgAction, Parser, Subcommand};
 use serde::Deserialize;
-use serde_json::{from_str, to_string};
-use sqlx::{Connection, Executor, FromRow, PgConnection};
+use serde_json::from_str;
+use sqlx::{Connection, Executor, PgConnection};
+use std::collections::HashMap;
 use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
 use tokio::sync::mpsc;
-use tokio::sync::mpsc::Sender;
+use tokio::sync::mpsc::{Receiver, Sender};
 use tracing::{debug, error, info, trace};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::{EnvFilter, fmt};
@@ -96,12 +97,7 @@ fn parse(message: &str) -> anyhow::Result<UpdateMessage> {
     });
 }
 
-async fn decipher(stdout_filter: EnvFilter, input: &Path, tx: Sender<UpdateMessage>) {
-    let stdout_layer = fmt::Layer::default()
-        .compact()
-        .with_writer(std::io::stderr)
-        .with_filter(stdout_filter);
-    tracing_subscriber::registry().with(stdout_layer).init();
+async fn decipher(input: &Path, tx: Sender<UpdateMessage>) {
     let file = fs::File::open(input).unwrap();
     let mut update_count = 0;
     for (line, _line_count) in BufReader::new(file).lines().zip(0..) {
@@ -130,30 +126,40 @@ async fn decipher(stdout_filter: EnvFilter, input: &Path, tx: Sender<UpdateMessa
                 None => debug!("non-ascii message of length {}", message.len()),
             };
         }
+        if update_count % 1000 == 0 {
+            info!("processed {update_count}");
+        }
     }
     info!("update_count: {update_count}");
 }
 
-async fn create_metric(conn: &mut PgConnection, update: UpdateMessage) -> Result<(), sqlx::Error> {
+async fn create_metric(
+    metrics: &HashMap<(String, i32), i32>,
+    conn: &mut PgConnection,
+    update: UpdateMessage,
+) -> Result<(), sqlx::Error> {
     let time = DateTime::<Utc>::from_timestamp(update.time, 0).expect("Invalid UNIX timestamp");
     let values = update
         .metrics
         .into_iter()
         .zip(0..)
-        .map(|(metric, i)| match metric {
-            Some(value) => format!("($1, $2, {i}, {value})"),
-            None => format!("($1, $2, {i}, NULL)"),
+        .map(|(metric, i)| {
+            let id = metrics[&(update.path.clone(), i)];
+            match metric {
+                Some(value) => format!("({id}, $1, {value})"),
+                None => format!("({id}, $1, NULL)"),
+            }
         })
         .collect::<Vec<String>>()
         .join(",");
     let query_str = format!(
         "
-        INSERT INTO metrics (time, path, name, value)
+        INSERT INTO metrics (partition, time, value)
           VALUES {values}
         "
     );
 
-    let query = sqlx::query(&query_str).bind(time).bind(&update.path);
+    let query = sqlx::query(&query_str).bind(time);
     let result = conn.execute(query).await?;
     trace!("{:#?}", result);
     Ok(())
@@ -166,22 +172,70 @@ async fn create_table() -> Result<(), sqlx::Error> {
     let query = sqlx::query(
         "
     CREATE TABLE metrics (
-      time     TIMESTAMPTZ       NOT NULL,
-      path     TEXT              NOT NULL,
-      name     TEXT              NOT NULL,
-      value    DOUBLE PRECISION,
-      PRIMARY KEY (path, name, time)
-    ) WITH (
-      timescaledb.hypertable,
-      timescaledb.partition_column='time',
-      timescaledb.segmentby='path, name'
-    );
+      partition BIGINT            NOT NULL,
+      time      TIMESTAMPTZ       NOT NULL,
+      value     DOUBLE PRECISION,
+      PRIMARY KEY (partition, time)
+    ) PARTITION BY LIST(partition);
     ",
     );
     let conn_string = "postgres://postgres:password@localhost/postgres";
     let mut conn = PgConnection::connect(conn_string).await?;
     let result = conn.execute(query).await?;
     trace!("{:#?}", result);
+    Ok(())
+}
+
+async fn create_partitions(
+    mut rx: Receiver<UpdateMessage>,
+) -> Result<HashMap<(String, i32), i32>, sqlx::Error> {
+    let conn_string = "postgres://postgres:password@localhost/postgres";
+    let mut conn = PgConnection::connect(conn_string).await.unwrap();
+    let mut seen_metrics = HashMap::new();
+    let mut unique_id = 0;
+
+    while let Some(update) = rx.recv().await {
+        for (_metric, id) in update.metrics.iter().zip(0..) {
+            seen_metrics
+                .entry((update.path.clone(), id))
+                .or_insert(unique_id);
+            let query_str = format!(
+                "
+            CREATE TABLE metrics_{unique_id} PARTITION of metrics
+              FOR VALUES IN ({unique_id});
+            "
+            );
+            let query = sqlx::query(&query_str);
+            let result = conn.execute(query).await?;
+            trace!("{:#?}", result);
+            unique_id += 1;
+        }
+    }
+    // We really want to save this instead
+    //  host     TEXT              NOT NULL,
+    //  service  TEXT              NOT NULL,
+    Ok(seen_metrics)
+}
+
+async fn create_metrics(metrics: &HashMap<(String, i32), i32>, mut rx: Receiver<UpdateMessage>) {
+    let mut count = 0;
+    let conn_string = "postgres://postgres:password@localhost/postgres";
+    let mut conn = PgConnection::connect(conn_string).await.unwrap();
+    while let Some(update) = rx.recv().await {
+        count += 1;
+        if let Err(e) = create_metric(&metrics, &mut conn, update).await {
+            error!("could not create metric {e:?}");
+        }
+    }
+    info!("finished processing");
+}
+
+async fn create_metrics_from_file(
+    input: &Path,
+    metrics: &HashMap<(String, i32), i32>,
+) -> Result<(), sqlx::Error> {
+    let (tx, rx) = mpsc::channel::<UpdateMessage>(32);
+    let (_send, _cons) = tokio::join!(decipher(&input, tx), create_metrics(metrics, rx),);
     Ok(())
 }
 
@@ -193,28 +247,18 @@ async fn main() -> Result<(), sqlx::Error> {
         1 => "debug",
         _ => "trace",
     });
-    let (tx, mut rx) = mpsc::channel::<UpdateMessage>(32);
-
+    let stderr_layer = fmt::Layer::default()
+        .compact()
+        .with_writer(std::io::stderr)
+        .with_filter(filter);
+    tracing_subscriber::registry().with(stderr_layer).init();
     let Command::Decipher { input } = arguments.command;
-    let sender = tokio::spawn(async move { decipher(filter, &input, tx).await });
+
+    let (tx, rx) = mpsc::channel::<UpdateMessage>(32);
     create_table().await?;
-    let consumer = tokio::spawn(async move {
-        let mut count = 0;
-        let conn_string = "postgres://postgres:password@localhost/postgres";
-        let mut conn = PgConnection::connect(conn_string).await.unwrap();
-        while let Some(update) = rx.recv().await {
-            count += 1;
-            if let Err(e) = create_metric(&mut conn, update).await {
-                error!("could not create metric {e:?}");
-            }
-            if count % 100000 == 0 {
-                info!("processed {count}");
-            }
-        }
-        info!("finished processing");
-    });
-    let (cons, send) = tokio::join!(consumer, sender);
-    cons.unwrap();
-    send.unwrap();
-    Ok(())
+    let (_send, cons) = tokio::join!(decipher(&input, tx), create_partitions(rx));
+    let metrics = cons.unwrap();
+    info!("finished partition");
+
+    create_metrics_from_file(&input, &metrics).await
 }
